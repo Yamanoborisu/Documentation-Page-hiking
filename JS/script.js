bd26cda076bb0cfe239a91622179c68360d21728
Yamanoborisu <65376569+Yamanoborisu@users.noreply.github.com>
2020-08-06 15:33:32 +0900

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+$('#landformsDetail').click(function() {
+$('#hide').toggle({
+   duration: 'slow',
+   easing: 'linear'
+});
+});
+
+$('#cloudsDetail').click(function() {
+$('#cloudHide').toggle({
+   duration: 'slow',
+   easing: 'linear'
+});
+});
+
+$('#staff_show').click(function() {
+$('#nav_hide').toggle({
+   duration: 'quick',
+   easing: 'linear'
+});
+});
